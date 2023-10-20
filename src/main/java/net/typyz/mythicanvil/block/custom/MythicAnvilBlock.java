3f7ffa6766dc042f2c41a62cd4a61a06b5109c3c
TYPyz <marchetti.alessandro2d@gmail.com>
2023-10-20 16:59:53 +0200

    Created complex interaction with MythicAnvil, now I can turn 2 items into a third item by right-clicking on the MythicAnvil block

--- src/main/java/net/typyz/mythicanvil/block/custom/MythicAnvilBlock.java
+++ src/main/java/net/typyz/mythicanvil/block/custom/MythicAnvilBlock.java
@@ -1,12 +1,31 @@
 package net.typyz.mythicanvil.block.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 
 public class MythicAnvilBlock extends Block {
@@ -26,6 +45,7 @@ public class MythicAnvilBlock extends Block {
         super(pProperties);
     }
 
+
     @Override
     public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
         return SHAPE;
@@ -33,5 +53,60 @@ public class MythicAnvilBlock extends Block {
     @Override
     public RenderShape getRenderShape(BlockState pState) {
         return RenderShape.MODEL;
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level worldIn, BlockPos pos, Player player, InteractionHand handIn, BlockHitResult hit) {
+        if (worldIn.isClientSide()) {
+            // This code only runs on the client side
+            return InteractionResult.SUCCESS;
+        }
+
+        ItemStack heldItem = player.getItemInHand(handIn);
+        boolean itemInMainHand = false;
+        boolean hasFirstIngredient = false;
+        boolean hasSecondIngredient = false;
+        List<ItemEntity> itemsToRemove = new ArrayList<>();
+
+        if (handIn == InteractionHand.MAIN_HAND) {
+            if (heldItem.getItem() == Items.DIAMOND_AXE) {
+                // Check if the player is holding correct tool
+                itemInMainHand = true;
+
+                List<ItemEntity> itemsOnBlock = worldIn.getEntitiesOfClass(ItemEntity.class, new AABB(pos.getX(), pos.getY() + 1.5D, pos.getZ(), pos.getX() + 1.0D, pos.getY() + 1.5D, pos.getZ() + 1.0D));
+
+                for (ItemEntity itemEntity : itemsOnBlock) {
+                    Item item = itemEntity.getItem().getItem();
+                    if (item == Items.EMERALD) {
+                        // Check for first ingredient
+                        hasFirstIngredient = true;
+                        itemsToRemove.add(itemEntity);
+                    } else if (item == Items.COAL) {
+                        // Check for second ingredient
+                        hasSecondIngredient = true;
+                        itemsToRemove.add(itemEntity);
+                    }
+                }
+
+            }
+            if (itemInMainHand && hasFirstIngredient && hasSecondIngredient) {
+                worldIn.playSound(null, pos, SoundEvents.LIGHTNING_BOLT_THUNDER, SoundSource.BLOCKS, 1.0F, 1.0F);
+                for (ItemEntity itemEntity : itemsToRemove) {
+                    itemEntity.remove(Entity.RemovalReason.KILLED);
+                }
+
+                LightningBolt lightningbolt = EntityType.LIGHTNING_BOLT.create(worldIn);
+                if (lightningbolt != null) {
+                    worldIn.addFreshEntity(lightningbolt);
+                }
+                ItemStack diamondStack = new ItemStack(Items.DIAMOND);
+                ItemEntity diamondEntity = new ItemEntity(worldIn, pos.getX() + 0.5, pos.getY() + 1.5, pos.getZ() + 0.5, diamondStack);
+                worldIn.addFreshEntity(diamondEntity);
+            } else {
+                worldIn.playSound(null, pos, SoundEvents.ANVIL_LAND, SoundSource.BLOCKS, 1.0F, 1.0F);
+            }
+
+        }
+        return InteractionResult.SUCCESS;
     }
 }
